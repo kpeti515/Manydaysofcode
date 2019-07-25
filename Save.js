4bafbc9ba2b03c28dcc67b1a42150c185e2d8a61
Lóránd Kállai <lorandkallai@gmail.com>
2019-07-25 21:46:42 +0200

    Day3+Day4

---
+++ Save.js
@@ -0,0 +1,13 @@
+﻿function SaveData(passForm) {
+
+    set fso = CreateObject("Scripting.FileSystemObject");
+    set s = fso.CreateTextFile("saveddatas.txt", True);
+
+    var Name = document.getElementById('Name');
+    var Email = document.getElementById('email');
+
+    s.writeline("Name :" + Name);
+    s.writeline("E-mail :" + Email);
+
+    s.writeline("-----------------------------");
+    s.Close();
